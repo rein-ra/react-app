aff15a41cae8f8da87453e4a191866228dcbc11f
Romil Rõbtšenkov <romil@testlio.com>
2020-10-14 08:24:05 +0300

    added previous week example

---
+++ react-app/src/App copy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+import ööö
+import './App.css';
+
+const App = () => (
+  <div>
+    <Button>Button</Button>
+  </div>
+);
+
+export default App;
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout, Row, Col } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Header, Content } = Layout;
+
+const App = () => (
+  <Layout>
+    <Header className={'app-header'}>App</Header>
+    <Content className={'app-content'}>
+      <Row>
+        <Col span={12}>Left</Col>
+        <Col span={12}>Right</Col>
+      </Row>
+    </Content>
+  </Layout>
+);
 
 export default App;
